3a70341168bdc36d15c8836709c7465b813b26c1
@MR <49095622+MartaR9@users.noreply.github.com>
2021-05-10 01:27:06 +0200

    Initial commit

--- Utilities/src/hr/algebra/factory/UrlConnectionFactory.java
+++ Utilities/src/hr/algebra/factory/UrlConnectionFactory.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hr.algebra.factory;
 
 import java.io.IOException;
@@ -10,10 +6,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-/**
- *
- * @author dnlbe
- */
+
 public class UrlConnectionFactory {
     
     public static HttpURLConnection getHttpUrlConnection(String path, int timeout, String requestMethod) throws MalformedURLException, IOException {
